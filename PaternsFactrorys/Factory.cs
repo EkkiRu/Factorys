fdd1d5c530c28200e293eef02253cb9d3630b7fd
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2021-01-19 16:58:59 +0600

    Homework
    Homework

---
+++ PaternsFactrorys/Factory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaternsFactrorys
+{
+    public interface IBread
+    {
+        void Bake();
+        void Provide();
+    }
+    class Bun : IBread
+    {
+        public void Provide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Bake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Loaf : IBread
+    {
+        public void Provide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Bake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Cake : IBread
+    {
+        public void Provide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Bake()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public interface IBreadFactory
+    {
+        IBread CreateFan();
+    }
+
+    public class TableFanFactory : IBreadFactory
+    {
+        public IBread CreateFan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class AutoFanFactory : IBreadFactory
+    {
+        public IBread CreateFan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PCFanFactory : IBreadFactory
+    {
+        public IBread CreateFan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HairFanFactory : IBreadFactory
+    {
+        public IBread CreateFan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
